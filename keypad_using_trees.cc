64e3b68c9579d8492a39a95b3385f746d7796bd6
spdev7 <sauravprakash7@outlook.com>
2018-10-15 08:17:47 +0530

    keypad

--- keypad_using_trees.cc
+++ keypad_using_trees.cc
@@ -17,7 +17,8 @@ void keypad(string input,string output){
         for(int j=0;j<)
        keypad(input.substr(0,1),s1[]+output);
       
-}sdskdjfs;dldfds
+}
+
 
 int main(){
   string input;
@@ -25,3 +26,33 @@ int main(){
   string output ="";
   keypad(input,output);
 }
+
+// void printKeypad(int num){
+//     /*
+//     Given an integer number print all the possible combinations of the keypad. You do not need to return anything just print them.
+//     */
+//     string s1 = to_string(num);
+//     string output = "";
+//     printKeypad(s1,output);
+  
+// }
+// void printKeypad(string input,string output)
+// {
+//     string s1[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+//      string temp;
+//     //base case 
+//     if(input.empty()){
+//        cout<<output<<endl;
+//        return;
+//     }
+
+//     int value = input.substr(0,1);
+//     int remaining = input.substr(1);
+//     int int_value = atoi(value);
+//     //main function call
+//       string value1=s1[int_value];
+//       for(int i=0;i<value1.length();i++)
+//       {   string xoxo = s1[value1][i];
+//        printKeypad(remaining,output+xoxo);
+//       }
+// }
