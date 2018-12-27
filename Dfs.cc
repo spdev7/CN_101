fac39e7a2d1f10518453722a6bdc52494dee4331
spdev7 <sauravprakash7@outlook.com>
2018-12-27 16:47:55 +0530

    ..

---
+++ Dfs.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int e,n;
+    //e---->no of edges && n--->no of vertices
+    cin>>e>>n;
+
+    int ** Edges = new int*[n];  //Creating a 2D array
+    for(int i=0;i<n;i++){
+        Edges[i]=new int[n];
+        for(int j=0;j<n;j++)
+        {
+             Edges
+        }
+    }
+
+
+
+
+    return 0;
+}
