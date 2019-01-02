28b93bd14f8c536e9b1b69d1cd33355cfd90b623
spdev7 <sauravprakash7@outlook.com>
2019-01-02 23:29:17 +0530

    ref

--- Lecture 4-Number_Theory/findprime.cc
+++ Lecture 4-Number_Theory/findprime.cc
@@ -5,10 +5,10 @@ using namespace std;
 int main()
 {
     int a;
-    int count;
+    int count=1;
     cin>>a;
     while(a>0){
-        if(!(a/2 || a/3 || a/5 || a/7))
+        if(!(a/2 | a/3 | a/5 | a/7))
     {
         count++;
     }
